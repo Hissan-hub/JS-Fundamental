dc4aad01c0b18d751886727046a843be69a343a7
cyuzuzo christian <christianhissan@gmail.com>
2025-11-08 11:52:06 +0200

    Fix: handle no argument & proper factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,9 +1,19 @@
 function factorial(n) {
-  if (isNaN(n) || n === 0) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
-const num = Number(process.argv[2]);
-console.log(factorial(num));
+const arg = process.argv[2];
+
+if (arg === undefined) {
+  console.log("No argument");
+} else {
+  const num = Number(arg);
+  console.log(factorial(num));
+}
+
