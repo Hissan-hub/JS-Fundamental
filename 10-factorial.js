3516d2a8758fc57de940dd2fcb24c7f01523f4fd
cyuzuzo christian <christianhissan@gmail.com>
2025-11-08 11:47:51 +0200

    Fix: recursive factorial implementation

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,10 +1,9 @@
-const arg = Number(process.argv[2]);
-
 function factorial(n) {
-  if (isNaN(n) || n <= 1) {
+  if (isNaN(n) || n === 0) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
-console.log(factorial(arg));
+const num = Number(process.argv[2]);
+console.log(factorial(num));
